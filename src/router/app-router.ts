2e8eb79bfe82ba6b56bc96daf879b2e3fcf2ff79
fanil279 <fanilramakaev@gmail.com>
2025-06-17 14:28:47 +0500

    fix: order of routes changed

--- src/router/app-router.ts
+++ src/router/app-router.ts
@@ -7,6 +7,7 @@ import { getMemberPermissions } from '../middlewares/getMemberPermissions';
 let router = express.Router();
 
 router.get('/me', isAuth, userController.getMe);
+router.delete('/projects/:projectId/members/me', isAuth, getMemberPermissions, userController.leaveProject);
 router.post('/new-project', isAuth, userController.createNewProject);
 router.get('/projects', isAuth, userController.getProjects);
 router.post('/project-details', isAuth, userController.ProjectDetails);
@@ -14,7 +15,6 @@ router.delete('/delete-project/:projectId', isAuth, getMemberPermissions, userCo
 router.put('/update-project/:projectId', isAuth, userController.updateProject);
 router.patch('/projects/:projectId/members/:memberId', isAuth, userController.changeTeamMemberRole);
 router.delete('/projects/:projectId/members/:memberId', isAuth, getMemberPermissions, userController.removeTeamMember);
-router.delete('/projects/:projectId/members/me', isAuth, getMemberPermissions, userController.leaveProject);
 router.post('/enhance', isAuth, aiController.EnhanceText);
 
 export default router;
