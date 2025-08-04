6ee5c012c62df418416f0d5bbcd167eb930fc421
shodievSA <shodiev.s.a@gmail.com>
2025-08-04 16:10:02 +0500

    replace '+5:00' to '+05:00'

--- src/clients/sequelize.ts
+++ src/clients/sequelize.ts
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(DB_NAME!, DB_USERNAME!, DB_PASSWORD!, {
     // this is for docker - port: 5432,
     dialect: 'postgres',
 	logging: false, // Disable logging; default: console.log
-    timezone: '+5:00', // Uzbekistan(Tashkent) time in Postgres
+    timezone: '+05:00', // Uzbekistan(Tashkent) time in Postgres
     dialectOptions: { 
         useUTC : false,
     }
