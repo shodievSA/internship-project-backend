0f52407a2c40789c6b9931a8158a1666352a7762
fanil279 <fanilramakaev@gmail.com>
2025-08-05 12:41:41 +0500

    added transaction

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -708,7 +708,8 @@ class ProjectService {
 			}
 
 			const deletedProjectCount = await models.Project.destroy({
-				where: { id: projectId }
+				where: { id: projectId },
+				transaction
 			});
 		
 			if (deletedProjectCount === 0) {
@@ -1233,7 +1234,8 @@ class ProjectService {
 
 			const assignedBy = await models.ProjectMember.findOne({
 				where: { userId: userId },
-				attributes: ['id']
+				attributes: ['id'],
+				transaction
 			});
 
 			if (!assignedBy) throw new Error("");
@@ -1264,6 +1266,7 @@ class ProjectService {
                     assignedBy: assignedBy.id
                 },
                 force: true,
+				transaction
             });
     
             if (!isDeleted) { 
@@ -2090,7 +2093,7 @@ class ProjectService {
 
 	}
 
-    async deleteSprint(projectId: number, sprintId:number): Promise<void> {
+    async deleteSprint(projectId: number, sprintId: number): Promise<void> {
 
 		const transaction: Transaction = await sequelize.transaction();
 
@@ -2127,7 +2130,8 @@ class ProjectService {
 				where: { 
                     id: sprintId,
                     projectId: projectId
-                }
+                },
+				transaction
 			});
 		
 			if (deletedSprint === 0) {
