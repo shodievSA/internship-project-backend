ac3fe3c6883d6933784d785b787b5c70d2378eeb
shodievSA <shodiev.s.a@gmail.com>
2025-08-21 17:23:12 +0500

    improve error handling in authController

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,58 +1,51 @@
+import { AppError } from '@/types';
 import { Request, Response, NextFunction } from 'express';
 import passport from 'passport';
 
 const passportAuth = passport.authenticate('google', {
-
 	scope: [
 		'profile', 
 		'email',
 		'https://www.googleapis.com/auth/gmail.readonly',
         'https://www.googleapis.com/auth/contacts.readonly'
 	]
-
 });
 
 const passportRedirect = passport.authenticate('google', {
-
 	successRedirect: `${process.env.FRONTEND_URL}/projects`,
 	failureRedirect: `${process.env.FRONTEND_URL}/sign-in`
-
 });
 
-function logout(
-	req: Request, 
-	res: Response, 
-	next: NextFunction
-): void {
+function logout(req: Request, res: Response, next: NextFunction): void {
 
-	req.logout((err: any) => {
+	try {
 
-		if (err) return next(err);
+		req.logout((err: any) => {
 
-		req.session.destroy(err => { 
+			if (err) throw new AppError("Unexpected error occured while trying to log you out", 500, true);
 
-			if (err){ 
+			req.session.destroy((err) => { 
 
-				console.error('Error destroying session:', err);
+				if (err) throw new AppError("Unexpected error occured while trying to destroy your session", 500, true);
 
-				return res.status(500).json({ message: 'Internal server error' });
+				res.clearCookie('connect.sid', {
+					httpOnly: true,
+					sameSite: 'strict',
+					secure: process.env.NODE_ENV === 'production',
+					path: '/',
+				});
 
-			}
+				return res.status(204).json({ message: "Successfully logged you out!" });
 
-			res.clearCookie('connect.sid', {
-
-				httpOnly: true,
-				sameSite: 'strict',
-				secure: process.env.NODE_ENV === 'production',
-				path: '/',
-
-			})
-
-			return res.sendStatus(204);
-
-		})
+			});
 		
-	});
+		});
+
+	} catch (err) {
+
+		next(err);
+
+	}
 
 };
 
