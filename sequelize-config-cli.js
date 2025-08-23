6c0a5517a221f1f6fafea755347ca7bf317c17d8
shodievSA <shodiev.s.a@gmail.com>
2025-08-23 13:32:57 +0500

    remove ./config folder and rename config.js file into sequelize-config-cli.js; add new scripts into package.json file that perform database migrations when running npm run dev and npm start commands

--- sequelize-config-cli.js
+++ sequelize-config-cli.js
@@ -1,6 +1,6 @@
-import 'dotenv/config';
+require('dotenv').config();
 
-export default {
+module.exports = {
 	development: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
