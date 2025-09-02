760354bc690c14034acdf70e2895eba5ee9e8e26
Asad <asadrahmonberdiev@gmail.com>
2025-09-02 19:00:06 +0500

    fix: remove unused updatedTasks variable and related task history creation in markOverdueTasks function

--- src/services/cronService.ts
+++ src/services/cronService.ts
@@ -26,7 +26,7 @@ async function markOverdueTasks() {
     try {
         const tz = 'Asia/Tashkent'
 
-        const [updatedCount, updatedTasks] = await Task.update(
+        const [updatedCount] = await Task.update(
             { status: 'overdue' },
             {
                 where: {
@@ -39,7 +39,6 @@ async function markOverdueTasks() {
                        { status: ['ongoing', 'under review', 'rejected']},
                     ],
                 },
-                returning: true,
                 transaction: transaction,
             },
             
@@ -47,16 +46,6 @@ async function markOverdueTasks() {
 
         if (updatedCount > 0) {
             console.log(`Marked ${updatedCount} tasks as overdue.`);
-        }
-
-        for (const task of updatedTasks) { 
-
-            await models.TaskHistory.create({
-                taskId: task.id,
-                status: 'overdue',
-            },
-            {transaction : transaction}
-            )
         }
 
         await transaction.commit()
